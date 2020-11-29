740a1a7e24a6435377b2d9d466fbbb6276a83c95
augustolachini <augustolachini@gmail.com>
2020-11-29 11:35:49 -0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,5 +1,4 @@
 import os
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -22,46 +21,11 @@ app.layout = html.Div([
 
 @app.callback(dash.dependencies.Output('display-value', 'children'),
               [dash.dependencies.Input('dropdown', 'value')])
+
+
 def display_value(value):
     return 'You have selected "{}"'.format(value)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
     
-    
-
-## First Example
-
-# -*- coding: utf-8 -*-
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-
-app = dash.Dash()
-
-app.layout = html.Div(
-    html.Div([
-        html.H1(children='Hello World'),
-
-        html.Div(children='''
-            Dash: A web application framework for Python.
-        '''),
-
-        dcc.Graph(
-            id='example-graph',
-            figure={
-                'data': [
-                    {'x': [1, 2, 3], 'y': [10, 1, 2], 'type': 'bar', 'name': 'SF'},
-                    {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-                ],
-                'layout': {
-                    'title': 'Dash Data Visualization'
-                }
-            }
-        ),
-
-    ])
-)
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
